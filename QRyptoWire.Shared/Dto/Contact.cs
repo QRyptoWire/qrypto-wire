e00ad03a4f216a562f6377444ec17d2ea68a3e9f
Konrad <konrjas@gmail.com>
2015-10-22 19:09:48 +0200

    dto definitions

---
+++ QRyptoWire.Shared/Dto/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace QRyptoWire.Shared.Dto
+{
+    public class Contact
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string PublicKey { get;  set; }
+    }
+}
