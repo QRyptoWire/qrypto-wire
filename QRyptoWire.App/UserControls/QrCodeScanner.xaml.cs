8621cd6d41e374128ba0f6c4f813acca39962098
Voytasify <qdirqa@gmail.com>
2015-11-01 14:19:28 +0100

    Add camera preview initialization

--- QRyptoWire.App/UserControls/QrCodeScanner.xaml.cs
+++ QRyptoWire.App/UserControls/QrCodeScanner.xaml.cs
@@ -22,6 +22,7 @@ using Windows.Media;
 using QRyptoWire.App.Annotations;
 using QRyptoWire.Shared.Dto;
 using QRyptoWire.Core.UserExceptions;
+using Panel = Windows.Devices.Enumeration.Panel;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -29,10 +30,72 @@ namespace QRyptoWire.App.UserControls
 {
     public sealed partial class QrCodeScanner : UserControl
     {
+        private MediaCapture _mediaCapture;
 
         public QrCodeScanner()
         {
             this.InitializeComponent();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await InitializeCameraPreviewAsync();
+            await StartCameraPreview();
+        }
+
+        private async Task InitializeCameraPreviewAsync()
+        {
+            var cam = await GetBackCamera();
+
+            if (cam == null)
+            {
+                cam = await GetFrontCamera();
+
+                if (cam == null)
+                {
+                    throw new Exception();
+                }
+            }
+
+            _mediaCapture = new MediaCapture();
+
+            await _mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings()
+            {
+                VideoDeviceId = cam.Id,
+                AudioDeviceId = "",
+                PhotoCaptureSource = PhotoCaptureSource.VideoPreview,
+                StreamingCaptureMode = StreamingCaptureMode.Video,
+            });
+        }
+
+        private async Task StartCameraPreview()
+        {
+            cameraPreview.Source = _mediaCapture;
+            await _mediaCapture.StartPreviewAsync();
+        }
+
+        private async Task<DeviceInformation> GetBackCamera()
+        {
+            var camList = await GetCameraDevices();
+
+            return (from cam in camList
+                    where cam.EnclosureLocation != null && cam.EnclosureLocation.Panel == Panel.Back
+                    select cam).FirstOrDefault();
+
+        }
+
+        private async Task<DeviceInformation> GetFrontCamera()
+        {
+            var camList = await GetCameraDevices();
+
+            return (from cam in camList
+                    where cam.EnclosureLocation != null && cam.EnclosureLocation.Panel == Panel.Front
+                    select cam).FirstOrDefault();
+        }
+
+        private async Task<DeviceInformationCollection> GetCameraDevices()
+        {
+            return await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
         }
 
     }
