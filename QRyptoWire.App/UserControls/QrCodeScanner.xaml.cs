c83f1cdfbd9bbc309d9b9a33d557a828c8007481
Voytasify <qdirqa@gmail.com>
2015-11-01 18:20:16 +0100

    Remove unused using directives

--- QRyptoWire.App/UserControls/QrCodeScanner.xaml.cs
+++ QRyptoWire.App/UserControls/QrCodeScanner.xaml.cs
@@ -1,28 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Media.Capture;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using System.Windows;
 using Windows.Devices.Enumeration;
-using Windows.Media;
 using Windows.Media.MediaProperties;
 using Windows.Storage;
-using QRyptoWire.App.Annotations;
-using QRyptoWire.Shared.Dto;
 using QRyptoWire.Core.CustomExceptions;
 using Panel = Windows.Devices.Enumeration.Panel;
 
