5fa910c2e5c42ea067de8bf8ad67978a067c4bb4
Voytasify <qdirqa@gmail.com>
2015-11-18 20:45:42 +0100

    Create QrService

---
+++ QRyptoWire.App.WPhone/PhoneImplementations/QrService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QRyptoWire.Core.Services;
+using QRyptoWire.Shared.Dto;
+
+namespace QRyptoWire.App.WPhone.PhoneImplementations
+{
+    class QrService : IQrService
+    {
+        public void GenerateQrCode(string contactName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ParseQrCode(string qrData, out Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
