e1e298006fa677e241e6e048e674560fd56e338d
Voytasify <qdirqa@gmail.com>
2015-11-20 13:43:37 +0100

    Create local db context

---
+++ QRyptoWire.App.WPhone/Models/QryptoDb.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace QRyptoWire.App.WPhone.Models
+{
+    [Database(Name = "QRyptoDb")]
+    public class QRyptoDb : DataContext
+    {
+        public Table<UserItem> Users;
+        public Table<MessageItem> Messages;
+        public Table<ContactItem> Contacts;
+
+        public const string ConnectionString = "DataSource='QryptoDb.sdf';Password={0};";
+
+        public QRyptoDb(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
