0f4deb93aeb6d6315b2861db42f588f06e205dc4
Konrad <konrjas@gmail.com>
2015-11-23 20:35:35 +0100

    loading overlay

--- QRyptoWire.App.WPhone/Views/QryptoWirePage.cs
+++ QRyptoWire.App.WPhone/Views/QryptoWirePage.cs
@@ -1,5 +1,10 @@
-﻿using Cirrious.MvvmCross.WindowsPhone.Views;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Cirrious.MvvmCross.WindowsPhone.Views;
 using Microsoft.Phone.Shell;
+using QRyptoWire.App.WPhone.Converters;
+using QRyptoWire.Core.ViewModels;
 
 namespace QRyptoWire.App.WPhone.Views
 {
@@ -7,10 +12,41 @@ namespace QRyptoWire.App.WPhone.Views
 	{
 		protected QryptoWirePage()
 		{
+			
             Loaded += (sender, args) =>
             {
                 SystemTray.IsVisible = false;
-            };
-        }
+
+				var dc = DataContext as QryptoViewModel;
+				if (dc != null)
+				{
+					var pageContent = Content as FrameworkElement;
+						if(pageContent == null)
+							return;
+
+					var rootGrid = new Grid
+					{
+						RowDefinitions = { new RowDefinition { Height = GridLength.Auto } }
+					};
+
+					//var binding = new Binding()
+					//{
+					//	Source = dc.Working,
+					//	Converter = new BoolToVisibilityConverter(),
+					//	FallbackValue = false
+					//};
+
+					var overlay = new WorkingOverlay();
+					Grid.SetRow(overlay, 1);
+					Grid.SetRow(pageContent, 1);
+					//BindingOperations.SetBinding(overlay, VisibilityProperty, binding);
+
+					Content = rootGrid;
+
+					rootGrid.Children.Add(pageContent);
+					rootGrid.Children.Add(overlay);
+				}
+			};
+		}
 	}
 }
--- QRyptoWire.Core/ViewModels/QryptoViewModel.cs
+++ QRyptoWire.Core/ViewModels/QryptoViewModel.cs
@@ -8,32 +8,49 @@ namespace QRyptoWire.Core.ViewModels
 	public abstract class QryptoViewModel : MvxViewModel
 	{
 		protected ManualResetEvent ResetEvent = new ManualResetEvent(false);
+		private bool _working;
+
+		public bool Working
+		{
+			get { return _working; }
+			set
+			{
+				_working = value;
+				RaisePropertyChanged();
+			}
+		}
 
 		public async void MakeApiCallAsync<TResult>(Func<TResult> call, Action<TResult> callback = null)
 		{
 			ResetEvent.Reset();
+			Working = true;
 
 			var res = await Task.Run(call);
 
 			if(res != null)
 				callback?.Invoke(res);
 
+			Working = false;
 			ResetEvent.Set();
 		}
 
 		public async void MakeApiCallAsync(Action call, Action callback = null)
 		{
 			ResetEvent.Reset();
+			Working = true;
 
 			await Task.Run(call);
 			callback?.Invoke();
 
+			Working = false;
 			ResetEvent.Set();
 		}
 
 		public async void MakeApiCallAsyncSafe(Action call, Action<bool> callback)
 		{
 			ResetEvent.Reset();
+			Working = true;
+
 			try
 			{
 				await Task.Run(call);
@@ -46,6 +63,7 @@ namespace QRyptoWire.Core.ViewModels
 			}
 			finally
 			{
+				Working = false;
 				ResetEvent.Set();
 			}
 		}
