2cb26e0936002cbf1229ef941cb1806cfbdf92bd
Konrad <konrjas@gmail.com>
2015-11-27 20:29:16 +0100

    added PasswordBox in LoginView, auto refreshing TextBoxes

--- QRyptoWire.App.WPhone/Views/LoginView.xaml.cs
+++ QRyptoWire.App.WPhone/Views/LoginView.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace QRyptoWire.App.WPhone.Views
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QRyptoWire.App.WPhone.Views
 {
     public partial class LoginView
     {
@@ -6,5 +11,24 @@
         {
             InitializeComponent();
         }
+
+	    public static readonly DependencyProperty DynamicTextBoxProperty =
+		    DependencyProperty.RegisterAttached("DynamicTextBox", typeof (bool), typeof (TextBox),
+			    new PropertyMetadata(default(bool), PropertyChangedCallback));
+
+		public static void SetCommand(DependencyObject obj, bool value)
+		{
+			obj.SetValue(DynamicTextBoxProperty, obj);
+		}
+
+		public static bool GetCommand(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(DynamicTextBoxProperty);
+		}
+
+		private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+	    {
+		    throw new NotImplementedException();
+	    }
     }
 }
