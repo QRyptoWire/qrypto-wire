da9c204ced3624e898ed34529c241d4a95be9e81
Konrad <konrjas@gmail.com>
2015-11-29 18:18:37 +0100

    added registration view

--- QRyptoWire.Core/ViewModels/RegistrationViewModel.cs
+++ QRyptoWire.Core/ViewModels/RegistrationViewModel.cs
@@ -1,8 +1,15 @@
-﻿using Cirrious.MvvmCross.ViewModels;
+﻿using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
 
 namespace QRyptoWire.Core.ViewModels
 {
 	public class RegistrationViewModel : MvxViewModel
 	{
+		public RegistrationViewModel()
+		{
+			GoCommand = new MvxCommand(() => ShowViewModel<LoginViewModel>());
+		}
+
+		public ICommand GoCommand { get; private set; }
 	}
 }
